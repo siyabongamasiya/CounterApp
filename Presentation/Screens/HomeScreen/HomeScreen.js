ec0982e58228166d8938fc2db1f8808f45ce05a9
Siyabonga <siyabongakhanyile76@gmail.com>
2025-01-22 01:43:58 +0200

    fixed architecture and setup

---
+++ Presentation/Screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,7 @@
+import { View } from "react-native"
+
+export default HomeScreen = () => {
+    return(
+        <View></View>
+    )
+}
---
+++ Presentation/Styles/ColorStyles.js
@@ -0,0 +1,8 @@
+export const primary = "007BFF"
+export const onprimary = "000000"
+
+export const secondary = "28A745"
+export const onsecondary = "000000"
+
+export const tertiary = "F8F9FA"
+export const ontertiary = "000000"
