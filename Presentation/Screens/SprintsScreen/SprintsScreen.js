310e1e969830d21794e6ac482511f9bd34af06f5
Siyabonga <siyabongakhanyile76@gmail.com>
2025-01-25 01:58:21 +0200

    added circular indicator

--- Presentation/Screens/SprintsScreen/SprintsScreen.js
+++ Presentation/Screens/SprintsScreen/SprintsScreen.js
@@ -1,5 +1,5 @@
-import { View,FlatList,Text} from "react-native"
-import { primaryvariant } from "../../Styles/ColorStyles"
+import { View,FlatList,ActivityIndicator,Text} from "react-native"
+import { primary, primaryvariant } from "../../Styles/ColorStyles"
 import { SprintItem } from "../../Components/Components";
 import { useEffect, useState } from "react";
 import { repo, SprintRepository } from "../../../Data/Repository/Repository";
@@ -9,9 +9,12 @@ import { SprintTimerStyles } from "../../Styles/Component_Styles";
 
 export default SprintsScreen = () => {
     const [data,setData] = useState([])
+    const [isloading,setisloading] = useState(true)
     const getSprints = async () => {
+        setisloading(true)
         const results = await repo.getAllSprints()
         setData(results)
+        setisloading(false)
     }
 
     useEffect(() => {
@@ -38,7 +41,12 @@ export default SprintsScreen = () => {
                 padding: 16,
               }}
            />
-        ) : (<View style = {{flex : 1,justifyContent : "center",alignItems : "center",backgroundColor : primaryvariant}}>
+        ) : (
+        //draw circular indicator if loading else show no sprints saved
+        isloading ? <View style = {{flex : 1,justifyContent : "center",alignItems : "center"}}>
+               <ActivityIndicator size={"large"} color= {primary} />
+            </View> :
+        <View style = {{flex : 1,justifyContent : "center",alignItems : "center",backgroundColor : primaryvariant}}>
             <Text style ={SprintTimerStyles.displayLarge}>No sprints Saved!!</Text>
         </View>)
         
